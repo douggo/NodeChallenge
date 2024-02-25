09f4d50300c0585f6586ebd1a075eef674420de2
Douglas Felipe da Silva <douggo@MacBook.local>
2024-02-25 00:48:49 -0300

    Configura o uso do Express.js no projeto

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const teste: string = "Hello World";
-console.log(teste);
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port:number = 3000;
+
+app.get("/", (request: Request, response: Response) => {
+  response.send("Iniciando desafio técnico para o Meu Crediário!");
+});
+
+app.listen(port, () => {
+  console.log(`API operando na porta ${port}!`);
+});
